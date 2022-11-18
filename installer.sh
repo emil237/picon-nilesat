ae7b5580276146a123ed8a03ab5f5d57a14ac21b
emil237 <marianatalaat1@gmail.com>
2022-11-18 16:07:05 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
-#!/bin/sh # 
- # # Command: wget https://raw.githubusercontent.com/emil237/picon-nilesat/main/installer.sh -qO - | /bin/sh # # ########################################### ###########################################  
+#!/bin/sh 
+#
 MY_URL="https://raw.githubusercontent.com/emil237/picon-nilesat/main"  
 echo "******************************************************************************************************************"
 echo "    download and install picon  "
@@ -8,13 +8,43 @@ echo "==========================================================================
 echo "         install picon-nilesat    "
 cd /tmp
 set -e 
-wget -q  "https://raw.githubusercontent.com/emil237/picon-nilesat/main/picon-nilesat.tar.gz"
+wget -q  "https://raw.githubusercontent.com/emil237/picon-nilesat/main/picon.zip"
 wait
-tar -xzf picon-nilesat.tar.gz  -C /
+unzip -qn ./picon.zip
+    rm -f /tmp/picon.zip
+wait
+
+if [ -d /media/mmc/picon/ ] ; then
+    cp -rf /tmp/picon/* /media/mmc/picon/
+	echo "Coping Picon To /media/mmc/picon/"
+    else
+    echo ""
+fi
+
+if [ -d /media/usb/picon/ ] ; then
+    cp -rf /tmp/picon/* /media/usb/picon/
+	echo "Coping Picon To /media/usb/picon/"
+    else
+    echo ""
+fi
+
+if [ -d /media/hdd/picon/ ] ; then
+    cp -rf /tmp/picon/* /media/hdd/picon/
+	echo "Coping Picon To /media/hdd/picon/"
+    else
+    echo ""
+fi
+
+if [ -d /usr/share/enigma2/picon/ ] ; then
+    cp -rf /tmp/picon/* /usr/share/enigma2/picon/
+	echo "Coping Picon To /usr/share/enigma2/picon/"
+    else
+    echo ""
+fi
 wait
 cd ..
 set +e
-rm -f /tmp/picon-nilesat.tar.gz
+rm -rf /tmp/picon
 echo "==========================================================================================================================="
 
 echo "****************************************************************************************************************************"
@@ -24,30 +54,10 @@ echo " "*********************************************************"
 	echo "********************************************************************************"
 echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
 sleep 4;
-	echo '========================================================================================================================='
-###########################################                                                                                                                  
-echo ". >>>>         RESTARING     <<<<"
+	echo '========================================================================================================================='                                                                                                             
 echo "**********************************************************************************"
 wait
-killall -9 enigma2
 exit 0
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
